08e1dba8d4bb6620be4df7833c32000aa12fd89e
TAD3V <37254451+TAD3V@users.noreply.github.com>
2019-07-08 02:37:12 +1000

    Update BlockFirstBlock.java

--- src/main/java/me/duat/redwood/block/BlockFirstBlock.java
+++ src/main/java/me/duat/redwood/block/BlockFirstBlock.java
@@ -1,58 +1,14 @@
 package me.duat.redwood.block;
 
 import net.minecraft.block.BlockLog;
-import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
-public class BlockFirstBlock extends BlockRotatedPillar {
-	public static final PropertyEnum<BlockLog.EnumAxis> LOG_AXIS = PropertyEnum.<BlockLog.EnumAxis>create("axis", BlockLog.EnumAxis.class);
-	
+public class BlockFirstBlock extends BlockLog {	
 	public BlockFirstBlock() {
-		super(Material.WOOD);
+		super();
 		setCreativeTab(CreativeTabs.MISC);
 		setSoundType(SoundType.WOOD);
 		setHardness(2.0F);
 	}
-	
-	@Override
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
-    {
-        return getStateFromMeta(meta).withProperty(BlockLog.LOG_AXIS, BlockLog.EnumAxis.fromFacingAxis(facing.getAxis()));
-    }
-
-    @Override
-    public IBlockState withRotation(IBlockState state, Rotation rot)
-    {
-        switch (rot)
-        {
-            case COUNTERCLOCKWISE_90:
-            case CLOCKWISE_90:
-
-                switch ((BlockLog.EnumAxis)state.getValue(LOG_AXIS))
-                {
-                    case X:
-                        return state.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);
-                    case Z:
-                        return state.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);
-                    default:
-                        return state;
-                }
-
-            default:
-                return state;
-        }
-    }
-
-    @Override public boolean canSustainLeaves(IBlockState state, net.minecraft.world.IBlockAccess world, BlockPos pos){ return true; }
-    @Override public boolean isWood(net.minecraft.world.IBlockAccess world, BlockPos pos){ return true; }
 }
